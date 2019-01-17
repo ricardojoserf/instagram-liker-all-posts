1187dfa8fe30695d0761c83bf972c7163d9278b1
Ricardo Ruiz <ricardojruiz93@gmail.com>
2019-01-17 14:24:21 +0100

    Ported to Python 3.x (working in both Python 2.x and 3.x)

--- README.md
+++ README.md
@@ -6,3 +6,21 @@ Like all posts of a user given the username in Instagram
 
 *python main.py -u YOUR_USERNAME -p YOUR_PASSWORD -t USERNAME_TARGET*
 
+
+## Requirements
+
+Python 2.x:
+
+```
+pip install -r install/requirements.txt
+```
+
+Python 3.x:
+
+```
+pip3 install -r install/requirements.txt
+```
+
+## Note
+
+Tested both in Python2.x (2.7.15rc1) and Python 3.x (3.6.7)
--- main.py
+++ main.py
@@ -15,16 +15,16 @@ def main():
 		api.login()
 		user_id = aux_funcs.get_id(str(args.target))
 		try:
-			print "Getting user feed. This may take a long time"
+			print ("Getting user feed. This may take a long time")
 			photos_ids = api.getTotalUserFeed(user_id)
-			print "Done! \n\nLiking all photos"
+			print ("Done! \n\nLiking all photos")
 			for p in photos_ids:
 				api.like(p.get("id"))
 				time.sleep( delay_seconds )
-			print "Done!"
+			print ("Done!")
 
 		except:
-			print "Unable to get user feed. Maybe account is private?"
+			print ("Unable to get user feed. Maybe account is private?")
 
 	except:
 		printUsage()
